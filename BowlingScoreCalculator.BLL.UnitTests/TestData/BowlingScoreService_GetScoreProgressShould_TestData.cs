87743d6f5668f8dabc4cea9d7d015018fcd09cea
Rex Lozano <romlozano@gmail.com>
2021-03-29 10:54:21 +1100

    Add test data for gutter ball game

--- BowlingScoreCalculator.BLL.UnitTests/TestData/BowlingScoreService_GetScoreProgressShould_TestData.cs
+++ BowlingScoreCalculator.BLL.UnitTests/TestData/BowlingScoreService_GetScoreProgressShould_TestData.cs
@@ -9,6 +9,7 @@ namespace BowlingScoreCalculator.BLL.UnitTests.TestData
         public static IEnumerable<object[]> GetTestData()
         {
             yield return new object[] { GetPerfectGameRequest, GetPerfectGameResponse };
+            yield return new object[] { GetGutterBallGameRequest, GetGutterBallGameResponse };
             yield return new object[] { GetSixFramesCompletedAllThrowsOneRequest, GetSixFramesCompletedAllThrowsOneResponse };
             yield return new object[] { GetSevenFramesCompletedWithSpareAndStrikesRequest, GetSevenFramesCompletedWithSpareAndStrikesResponse };
             // TODO: Handle gutter ball game
@@ -16,11 +17,20 @@ namespace BowlingScoreCalculator.BLL.UnitTests.TestData
 
         private static GetScoreProgressRequest GetPerfectGameRequest => new GetScoreProgressRequest
         {
-            PinsDowned = new List<int> { 10,10,10,10,10,10,10,10,10,10,10,10 }
+            PinsDowned = new List<int> { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 }
         };
         private static GetScoreProgressResponse GetPerfectGameResponse => new GetScoreProgressResponse
         {
             FrameProgressScores = new List<string> { "30", "60", "90", "120", "150", "180", "210", "240", "270", "300" },
+            GameCompleted = true
+        };
+        private static GetScoreProgressRequest GetGutterBallGameRequest => new GetScoreProgressRequest
+        {
+            PinsDowned = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+        private static GetScoreProgressResponse GetGutterBallGameResponse => new GetScoreProgressResponse
+        {
+            FrameProgressScores = new List<string> { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" },
             GameCompleted = true
         };
         private static GetScoreProgressRequest GetSixFramesCompletedAllThrowsOneRequest => new GetScoreProgressRequest
