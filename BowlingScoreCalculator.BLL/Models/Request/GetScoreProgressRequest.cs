03cc3910e3d312eed781760b65b12bb9b529e527
Rex Lozano <romlozano@gmail.com>
2021-03-29 07:47:01 +1100

    Add models

---
+++ BowlingScoreCalculator.BLL/Models/Request/GetScoreProgressRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BowlingScoreCalculator.BLL.Models.Request
+{
+    public class GetScoreProgressRequest
+    {
+        public IEnumerable<int> PinsDowned { get; set; }
+    }
+}
---
+++ BowlingScoreCalculator.BLL/Models/Response/GetScoreProgressResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace BowlingScoreCalculator.BLL.Models.Response
+{
+    public class GetScoreProgressResponse
+    {
+        public ICollection<string> FrameProgressScores { get; set; }
+        public bool GameCompleted { get; set; }
+    }
+}
