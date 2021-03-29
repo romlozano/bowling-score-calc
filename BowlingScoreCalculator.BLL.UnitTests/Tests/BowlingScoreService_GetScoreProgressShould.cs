5766967a7f14dc417e9512aaccf63f2103506d42
Rex Lozano <romlozano@gmail.com>
2021-03-29 11:23:39 +1100

    Add GetScoreProgress test

--- BowlingScoreCalculator.BLL.UnitTests/Tests/BowlingScoreService_GetScoreProgressShould.cs
+++ BowlingScoreCalculator.BLL.UnitTests/Tests/BowlingScoreService_GetScoreProgressShould.cs
@@ -2,6 +2,7 @@ using BowlingScoreCalculator.BLL.Models.Request;
 using BowlingScoreCalculator.BLL.Models.Response;
 using BowlingScoreCalculator.BLL.Services;
 using BowlingScoreCalculator.BLL.UnitTests.TestData;
+using BowlingScoreCalculator.Core.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections;
 
@@ -18,7 +19,7 @@ namespace BowlingScoreCalculator.BLL.UnitTests.Tests
             bowlingScoreService = new BowlingScoreService();
         }
 
-        [DynamicData(nameof(BowlingScoreService_GetScoreProgressShould_TestData.GetTestData), typeof(BowlingScoreService_GetScoreProgressShould_TestData), DynamicDataSourceType.Method)]
+        [DynamicData(nameof(BowlingScoreService_GetScoreProgressShould_TestData.GetReturnExpectedResponseTestData), typeof(BowlingScoreService_GetScoreProgressShould_TestData), DynamicDataSourceType.Method)]
         [DataTestMethod]
         public void GetScoreProgress_ReturnExpectedResponse(GetScoreProgressRequest request, GetScoreProgressResponse expectedResponse)
         {
@@ -26,6 +27,13 @@ namespace BowlingScoreCalculator.BLL.UnitTests.Tests
 
             Assert.AreEqual(expectedResponse.GameCompleted, result.GameCompleted);
             CollectionAssert.AreEqual((ICollection)expectedResponse.FrameProgressScores, (ICollection)result.FrameProgressScores);
+        }
+
+        [DynamicData(nameof(BowlingScoreService_GetScoreProgressShould_TestData.GetThrowBusinessArgumentExceptionTestData), typeof(BowlingScoreService_GetScoreProgressShould_TestData), DynamicDataSourceType.Method)]
+        [DataTestMethod]
+        public void GetScoreProgress_ThrowBusinessArgumentException_IfGetScoreProgressRequestIsInvalid(GetScoreProgressRequest request)
+        {
+            Assert.ThrowsException<BusinessArgumentException>(() => bowlingScoreService.GetScoreProgress(request));
         }
     }
 }
